5c9287951d2799427f5da3683ab89311b9ad0933
Nyah Check <check.nyah@gmail.com>
2018-01-24 12:04:33 -0800

    Added findNRooks solution.

--- src/solvers.js
+++ src/solvers.js
@@ -16,7 +16,10 @@
 
 
 window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
+  var solution = [[], []];
+  for (let i = 0; i < n; i++) {
+    solution[i][i] = 1;
+  }
 
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
   return solution;
